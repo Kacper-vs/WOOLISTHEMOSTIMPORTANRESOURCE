23937f4fd910e894dbac6a7ef6ae5a8d066e6c83
Kacper-vs <kappaflak@gmail.com>
2026-07-05 20:50:44 +0100

    pygame init

--- SRC/main.py
+++ SRC/main.py
@@ -1,7 +1,26 @@
 from speech import story
- 
+import pygame as pyg 
+import sys 
 
-def main():
-    story()
 
-main()
+pyg.init()
+
+screen = pyg.display.set_mode((800,600))
+pyg.display.set_caption("WOOLISTHEMOSTIMPORTANTRESOURCE")
+
+WHITE = (255,255,255)
+
+running = True 
+
+clock = pyg.time.Clock()
+
+while running:
+    for event in pyg.event.get():
+        if event.type == pyg.QUIT:
+            running = False 
+
+    screen.fill(WHITE)
+    pyg.display.flip()
+    clock.tick(60)
+pyg.quit()
+sys.exit()
